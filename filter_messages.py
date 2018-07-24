1bcb217e6864dbb7ab8f62f81cf2210ac784fbfe
jafow <jared.a.fowler@gmail.com>
2018-07-23 23:45:13 -0700

    clean up message walk code
    Ignore non-text messages
    
    cleanup, consolidate, move on

--- filter_messages.py
+++ filter_messages.py
@@ -2,54 +2,36 @@ import mailbox
 from email.generator import Generator
 
 mbox = mailbox.mbox('./data/Mail/messages.mbox')
-outbox = open('output04.txt', 'w')
+outbox = open('output_text_only_04.txt', 'w')
 g = Generator(outbox)
 
 
 def main(box):
-    preferred_content = {'text/plain', 'text/html'}
+    for i, m in box.iteritems():
+        if from_a_to_me(m) or from_me_to_a(m):
+            if m.is_multipart() is True:
+                for part in m.walk():
+                    descend(part)
+            else:
+                g.write(m.get_payload())
 
-    def walk_msg(m):
-        '''
-        iterates through a message object for multipart or lists of
-        messages and prints them out
-        '''
-        if m is None:
-            return None
 
-        if not from_a_to_me(m) or not from_me_to_a(m):
-            return None
+def descend(msg):
 
-        if m.is_multipart() is False:
-            # single message; write & return
-            return g.write(m.get_payload(decode=True))
-        else:
-            # multipart
-            for part in m.walk():
-                payload = m.get_payload(decode=True)
-                if isinstance(payload, list):
-                    g.write(g.flatten(payload))
-                    # for mm in payload:
-                    # walk_msg(mm)
-                elif payload is not None:
-                    return g.write(payload)
+    ''' climb down the message tree '''
 
-    for m in box.itervalues():
-        if m.is_multipart() is True:
-            walk_msg(m)
-        elif from_a_to_me(m) or from_me_to_a(m):
-            # try:
-                # g.write(p)
-            # except TypeError as e:
-                # print('error: ', e, ' for p: ', p)
-            g.write(m.get_payload())
-        else:
-            continue
+    preferred_content_types = {'text/plain', 'text/html', 'text/directory'}
+    p = msg.get_payload()
+    if not isinstance(p, list):
+        if msg.get_content_type() in preferred_content_types:
+            return g.write(p)
+    else:
+        for m in p:
+            descend(m)
 
 
 def from_a_to_me(m):
-    f = m_from(m)
-    return 'annie' in f.lower()
+    return 'annie' in m.get('from', None).lower()
 
 
 def from_me_to_a(m):
@@ -58,7 +40,7 @@ def from_me_to_a(m):
 
 
 def m_from(m):
-    x = m.get('from', None) or m.get('From', None) or m.get_from()
+    x = m.get('from', None) or m.get_from()
     return x
 
 
