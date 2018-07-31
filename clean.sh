f29f84f0deedd65724bb1a2fe4b9b05a85dfc0c7
jafow <jared.a.fowler@gmail.com>
2018-07-30 23:58:18 -0700

    adds more text cleaning

--- clean.sh
+++ clean.sh
@@ -4,4 +4,10 @@
 TARGET=$1;
 
 # 1. remove non-ascii and a few other things
-perl -i.bak -pe "s/[^[:ascii:]]//g; s/>+=20//g; s/(&#39;)/'/g" $TARGET
+perl -i.bak -pe "s/[^[:ascii:]]//g; s/>+=20|=[0-9]+//g; s/(&#39;)/'/g" $TARGET
+
+# 2. strip leading unicodes
+sed -i  -re '/=09|=30/d' \
+        -e '/(http)s?/d' \
+        -e 's/^(<+ )//g'
+$TARGET
